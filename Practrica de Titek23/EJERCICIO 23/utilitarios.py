5deba216d9b250ad49d997f78914818a5d0f1ee6
avsolorz <victoriasolorzano62@gmail.com>
2024-12-03 19:57:25 -0500

    arregle subtotal y descuento

--- Practrica de Titek23/EJERCICIO 23/utilitarios.py
+++ Practrica de Titek23/EJERCICIO 23/utilitarios.py
@@ -2,3 +2,14 @@ def calcularPromedio(val1,val2,val3):
     calculo=val1+val2+val3
     promedio1=calculo/3
     return promedio1
+
+def calcularSubtotal(val1,val2,val3):
+    subtotalSinDescuento =val1*val2
+    porcentajeDescuento = subtotalSinDescuento*val3 /100
+    subtotal = subtotalSinDescuento - porcentajeDescuento
+    return subtotal
+
+def calcularValorDescuento(val1,val2):
+    valorDescuento = val1*val2
+    calculardescuento = valorDescuento / 100
+    return calculardescuento
